7eebb2ecd092c0b39c43eed42620e1802358731c
Peter Mangiafico <peter@mangiafico.org>
2016-10-28 14:51:42 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+
+gem 'dor-services', '~> 5.12'
+gem 'lyber-core', '~> 4.0', '>= 4.0.3'
+gem 'robot-controller', '~> 2.0' # requires Resque
+gem 'pry', '~> 0.10.0'          # for bin/console
+gem 'slop', '>= 3.5.0'          # for bin/run_robot
+gem 'rake', '>= 10.3.2'
+gem 'dor-fetcher'
+gem 'retries'
+
+group :development, :test do
+  gem 'coveralls', require: false
+  gem 'rspec'
+  gem 'rubocop'
+  gem 'rubocop-rspec'
+  gem 'webmock'
+end
+
+group :development do
+  if File.exist?(mygems = File.join(ENV['HOME'],'.gemfile'))
+    instance_eval(File.read(mygems))
+  end
+  gem 'yard'
+  gem 'capistrano', '~> 3.0'
+  gem 'capistrano-bundler', '~> 1.1'
+  gem 'dlss-capistrano'
+end
---
+++ robots/dpgImage/goobi_notify.rb
@@ -0,0 +1,33 @@
+# Robot class to run under multiplexing infrastructure
+module Robots       # Robot package
+  module DorRepo    # Use DorRepo/SdrRepo to avoid name collision with Dor module
+    module DpgImage   # This is your workflow package name (using CamelCase)
+      class GoobiNotify # This is your robot name (using CamelCase)
+        # Build off the base robot implementation which implements
+        # features common to all robots
+        include LyberCore::Robot
+
+        def initialize
+          super('dor', Dor::Config.dpg.workflow_name, 'gooby-notify', check_queued_status: true) # init LyberCore::Robot
+        end
+
+        # `perform` is the main entry point for the robot. This is where
+        # all of the robot's work is done.
+        #
+        # @param [String] druid -- the Druid identifier for the object to process
+        def perform(druid)
+          LyberCore::Log.debug "goobi-notify working on #{druid}"
+          handler = proc do |exception, attempt_number, _total_delay|
+            LyberCore::Log.debug "#{exception.class} on dor-services-app update_marc_record call #{attempt_number} for #{@druid}" if attempt_number >= Dor::Config.dpg.max_tries
+          end
+
+          with_retries(max_tries: Dor::Config.dpg.max_tries, handler: handler, base_sleep_seconds: Dor::Config.dpg.base_sleep_seconds, max_sleep_seconds: Dor::Config.dpg.max_sleep_seconds) do |_attempt|
+            url = "#{Dor::Config.dor.service_root}/objects/#{@druid}/notify_goobi"
+            response = RestClient.post url,{}
+            response.code  
+          end                         
+        end
+      end
+    end
+  end
+end
